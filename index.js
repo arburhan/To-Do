2e60f477df1c9a01cc1ec3a26a531f02cf5841e2
arburhan <buthan190629@outlook.com>
2022-05-19 06:06:29 +0600

    create tasklist api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +9,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dlkda.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('Todo_Task').collection('Task_List');
+        app.get('/tasklist', async (req, res) => {
+            const tasklist = await taskCollection.find().toArray();
+            res.send(tasklist);
+            console.log('mongo connect')
+        })
+
+    }
+    finally {
+
+    }
+}
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello task!')
 })
 
 app.listen(port, () => {
